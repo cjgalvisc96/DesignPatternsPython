7bf918a1146522b6240c62f347b9a5e35937867e
cjgalvisc96 <cjgalvisc@unal.edu.co>
2021-05-24 23:49:59 -0500

    Refactor Bridge pattern(Add package form)

---
+++ StructuralPatterns/Bridge/circle_implementer.py
@@ -0,0 +1,16 @@
+"""A Circle Implementer"""
+from interface_shape_implementer import IShapeImplementer
+
+
+class CircleImplementer(IShapeImplementer):
+    """A Circle Implementer"""
+
+    def draw_implementation(self):
+        print("    ******")
+        print("  **      **")
+        print(" *          *")
+        print("*            *")
+        print("*            *")
+        print(" *          *")
+        print("  **      **")
+        print("    ******")
---
+++ StructuralPatterns/Bridge/interface_shape_implementer.py
@@ -0,0 +1,11 @@
+"""A Shape Implementor Interface"""
+from abc import ABCMeta, abstractmethod
+
+
+class IShapeImplementer(metaclass=ABCMeta):
+    """Shape Implementer"""
+
+    @staticmethod
+    @abstractmethod
+    def draw_implementation():
+        """The method that the refined abstractions will implement"""

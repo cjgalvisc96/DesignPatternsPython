299554b32810ae9c621c2d629d9799173138f242
Cristian Galvis <cgalvis@playvox.com>
2022-01-31 07:25:53 -0500

    Add Book/conceptualExamples/creationals/prototype.py

--- Book/conceptualExamples/creationals/prototype.py
+++ Book/conceptualExamples/creationals/prototype.py
@@ -29,7 +29,7 @@ class SomeComponent:
         new.__dict__.update(self.__dict__)
         return new
 
-    def __deepcopy__(self, *, memo={}) -> object:
+    def __deepcopy__(self, memo={}) -> object:
         some_list_of_objects = copy.deepcopy(self.some_list_of_objects, memo)
         some_circular_ref = copy.deepcopy(self.some_circular_ref, memo)
         new = self.__class__(
@@ -42,27 +42,87 @@ class SomeComponent:
 
 
 if __name__ == "__main__":
-    list_of_objects = [1, {1, 2, 4}, [1, 2, 3]]
+    list_of_objects = [1, {1, 2, 3}, [1, 2, 3]]
     circular_ref = SelfReferecingEntity()
-    component = SomeComponent(
-        some_int=23,
-        some_list_of_objects=list_of_objects,
-        some_circular_ref=circular_ref,
-    )
+    component = SomeComponent(some_int=23, some_list_of_objects=list_of_objects, some_circular_ref=circular_ref)
+    circular_ref.set_parent(parent=component)
 
     shallow_copied_component = copy.copy(component)
 
-    name_another_object = "Another object"
-    shallow_copied_component.some_list_of_objects.append(name_another_object)
-    if component.some_list_of_objects[-1] == name_another_object:
+    # Let's change the list in shallow_copied_component and see if it changes in
+    # component.
+    shallow_copied_component.some_list_of_objects.append("another object")
+    if component.some_list_of_objects[-1] == "another object":
         print(
-            "Adding elements to `shallow_copied_component`'s"
-            "some_list_of_objects adds it to `component`'s"
+            "Adding elements to `shallow_copied_component`'s "
+            "some_list_of_objects adds it to `component`'s "
             "some_list_of_objects."
         )
     else:
         print(
-            "Adding elements to `shallow_copied_component`'s"
-            "some_list_of_objects doesn't adds it to `component`'s"
+            "Adding elements to `shallow_copied_component`'s "
+            "some_list_of_objects doesn't add it to `component`'s "
             "some_list_of_objects."
         )
+
+    # Let's change the set in the list of objects.
+    component.some_list_of_objects[1].add(4)
+    if 4 in shallow_copied_component.some_list_of_objects[1]:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "changes that object in `shallow_copied_component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "doesn't change that object in `shallow_copied_component`'s "
+            "some_list_of_objects."
+        )
+
+    deep_copied_component = copy.deepcopy(component)
+
+    # Let's change the list in deep_copied_component and see if it changes in
+    # component.
+    deep_copied_component.some_list_of_objects.append("one more object")
+    if component.some_list_of_objects[-1] == "one more object":
+        print(
+            "Adding elements to `deep_copied_component`'s "
+            "some_list_of_objects adds it to `component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Adding elements to `deep_copied_component`'s "
+            "some_list_of_objects doesn't add it to `component`'s "
+            "some_list_of_objects."
+        )
+
+    # Let's change the set in the list of objects.
+    component.some_list_of_objects[1].add(10)
+    if 10 in deep_copied_component.some_list_of_objects[1]:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "changes that object in `deep_copied_component`'s "
+            "some_list_of_objects."
+        )
+    else:
+        print(
+            "Changing objects in the `component`'s some_list_of_objects "
+            "doesn't change that object in `deep_copied_component`'s "
+            "some_list_of_objects."
+        )
+
+    print(
+        f"id(deep_copied_component.some_circular_ref.parent): "
+        f"{id(deep_copied_component.some_circular_ref.parent)}"
+    )
+    print(
+        f"id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent): "
+        f"{id(deep_copied_component.some_circular_ref.parent.some_circular_ref.parent)}"
+    )
+    print(
+        "^^ This shows that deepcopied objects contain same reference, they "
+        "are not cloned repeatedly."
+    )
+

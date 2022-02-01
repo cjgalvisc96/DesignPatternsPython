fff88bd71fdb2ffacb5deab09b0ceb599ad7364d
Cristian Galvis <cgalvis@playvox.com>
2022-02-01 08:10:02 -0500

    Add Book/conceptualExamples/structurals/composite.py

---
+++ Book/conceptualExamples/structurals/composite.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Component(ABC):
+    @property
+    def parent(self) -> Component:
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent: Component):
+        self._parent = parent
+
+    def add(self, *, component: Component) -> None:
+        pass
+
+    def remove(self, *, component: Component) -> None:
+        pass
+
+    def is_composite(self) -> bool:
+        return False
+
+    @abstractmethod
+    def operation(self) -> str:
+        pass
+
+
+class Leaf(Component):
+    def operation(self) -> str:
+        return "Leaf"
+
+
+class Composite(Component):
+    def __init__(self) -> None:
+        self._children: List[Component] = []
+
+    def add(self, *, component: Component) -> None:
+        self._children.append(component)
+        component.parent = self
+
+    def remove(self, *, component: Component) -> None:
+        self._children.remove(component)
+        component.parent = self
+
+    def is_composite(self) -> bool:
+        return True
+
+    def operation(self) -> str:
+        results = []
+        for child in self._children:
+            results.append(child.operation())
+        return f"Branch({'+'.join(results)})"
+
+
+def client_code(*, component: Component) -> None:
+    print(f"RESULT: {component.operation()}", end="")
+
+
+def client_code2(*, component1: Component, component2: Component) -> None:
+    if component1.is_composite():
+        component1.add(component=component2)
+
+    print(f"RESULT: {component1.operation()}", end="")
+
+
+if __name__ == "__main__":
+    simple = Leaf()
+    print("Client: I've got a simple component:")
+    client_code(component=simple)
+    print("\n")
+
+    tree = Composite()
+    branch1 = Composite()
+    branch1.add(component=Leaf())
+    branch1.add(component=Leaf())
+
+    branch2 = Composite()
+    branch2.add(component=Leaf())
+
+    tree.add(component=branch1)
+    tree.add(component=branch2)
+
+    print("Client: Now I've got a composite tree:")
+    client_code(component=tree)
+    print("\n")
+
+    print(
+        "Client: I don't need to check the components classes even when managing the tree:"
+    )
+    client_code2(component1=tree, component2=simple)
